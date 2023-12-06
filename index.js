60364bea6f7e83c4a6a1003918d415a837fb7dd5
Elise Erickson <elisece717@gmail.com>
2023-12-06 13:25:17 -0500

    satisfies tests

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const originalArray = word.split("");
+  let newArray = [];
+
+  for(i = originalArray.length - 1; i >= 0; i--){
+    newArray.push(originalArray[i]);
+  }
+
+  return (newArray.join("") === word);
 }
 
 /* 
   Add your pseudocode here
+  
+  split string into array
+  iterate through array backward
+  to create new array
+  is new array = first array?
+  apparently since theyre objects theyre reference has to be equal
+  so convert back to a string and check that
 */
 
 /*
@@ -15,6 +30,11 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("aaaaaabbbbbbbbaaaaaa"));
 
   console.log("");
 
